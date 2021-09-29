7d136d330f0d6b556232db16a8d92450f0751398
Andreeesc <de_s_c@hotmail.com>
2021-09-29 16:53:18 -0300

    showing weekly timeframes

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -9,8 +9,31 @@ async function getData(){
 
 async function showData(){
   let cards = await getData()
+  let html = '' 
 
-  cards.forEach(card => {
-    console.log(card)
+  cards.forEach(card => {   
+    let htmlCards = `
+              <div class="card">
+                <div class="card-image">
+                  <figure>
+                    <img src="./assets/images/icon-${card.title.toLowerCase()}.svg" alt="">
+                  </figure>
+                </div>
+                <div class="card-info">
+                  <div class="card-title">
+                    <span>${card.title}</span>
+                    <img src="./assets/images/icon-ellipsis.svg" alt="">
+                  </div>
+                  <div class="card-hours">
+                    <h2>${card.timeframes.weekly.current}hrs</h2>
+                    <span>Last Week - ${card.timeframes.weekly.previous}hrs</span>
+                  </div>
+                </div>
+              </div>
+    `
+    html += htmlCards
   });
+
+  let card = document.querySelector('.cards');
+  card.innerHTML = html;
 }
