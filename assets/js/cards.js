e60f4548a7ce4c56050a8d7a18083f8e97226d60
Andreeesc <de_s_c@hotmail.com>
2021-09-30 15:38:16 -0300

    fix: showing data before click

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -1,4 +1,5 @@
 getData()
+showData('', 'week')
 
 async function getData(){
   const url = await fetch(`../../data.json`)
@@ -8,27 +9,50 @@ async function getData(){
 
 async function showData(period, date){
   const cards = await getData()
+
   const html = cards.map(({title, timeframes}) => {  
+    if(period == ''){
+      return `
+        <div class="card">
+          <div class="card-image">
+            <figure>
+              <img src="./assets/images/icon-${slugify(title)}.svg" alt="">
+            </figure>
+          </div>
+          <div class="card-info">
+            <div class="card-title">
+              <span>${title}</span>
+              <img src="./assets/images/icon-ellipsis.svg" alt="">
+            </div>
+            <div class="card-hours">
+              <h2>${timeframes.weekly.current}hrs</h2>
+              <span>Last ${date} - ${timeframes.weekly.previous}hrs</span>
+            </div>
+          </div>
+        </div>
+      `
+    } else {
+      return `
+        <div class="card">
+          <div class="card-image">
+            <figure>
+              <img src="./assets/images/icon-${slugify(title)}.svg" alt="">
+            </figure>
+          </div>
+          <div class="card-info">
+            <div class="card-title">
+              <span>${title}</span>
+              <img src="./assets/images/icon-ellipsis.svg" alt="">
+            </div>
+            <div class="card-hours">
+              <h2>${timeframes[period].current}hrs</h2>
+              <span>Last ${date} - ${timeframes[period].previous}hrs</span>
+            </div>
+          </div>
+        </div>
+      `
+    }
     
-    return `
-      <div class="card">
-        <div class="card-image">
-          <figure>
-            <img src="./assets/images/icon-${slugify(title)}.svg" alt="">
-          </figure>
-        </div>
-        <div class="card-info">
-          <div class="card-title">
-            <span>${title}</span>
-            <img src="./assets/images/icon-ellipsis.svg" alt="">
-          </div>
-          <div class="card-hours">
-            <h2>${timeframes[period].current}hrs</h2>
-            <span>Last ${date} - ${timeframes[period].previous}hrs</span>
-          </div>
-        </div>
-      </div>
-    `
   });
 
   let card = document.querySelector('.cards');
