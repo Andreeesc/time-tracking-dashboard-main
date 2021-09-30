3f19dadaef13d8ef3309839b9803693f0567b1de
Andreeesc <de_s_c@hotmail.com>
2021-09-30 11:59:11 -0300

    change on click idea

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -1,5 +1,5 @@
 getData()
-showData()
+// showData()
 
 async function getData(){
   let url = await fetch(`../../data.json`)
@@ -7,29 +7,34 @@ async function getData(){
   return response
 }
 
-async function showData(){
+async function showData(period){
   let cards = await getData()
   let html = '' 
 
-  cards.forEach(card => {   
+  console.log(period)
+
+  cards.forEach(card => {  
+    
+    console.log(card.timeframes.period)
+    
     let htmlCards = `
-              <div class="card">
-                <div class="card-image">
-                  <figure>
-                    <img src="./assets/images/icon-${card.title.toLowerCase()}.svg" alt="">
-                  </figure>
-                </div>
-                <div class="card-info">
-                  <div class="card-title">
-                    <span>${card.title}</span>
-                    <img src="./assets/images/icon-ellipsis.svg" alt="">
-                  </div>
-                  <div class="card-hours">
-                    <h2>${card.timeframes.weekly.current}hrs</h2>
-                    <span>Last Week - ${card.timeframes.weekly.previous}hrs</span>
-                  </div>
-                </div>
-              </div>
+      <div class="card">
+        <div class="card-image">
+          <figure>
+            <img src="./assets/images/icon-${card.title.toLowerCase()}.svg" alt="">
+          </figure>
+        </div>
+        <div class="card-info">
+          <div class="card-title">
+            <span>${card.title}</span>
+            <img src="./assets/images/icon-ellipsis.svg" alt="">
+          </div>
+          <div class="card-hours">
+            <h2>${card.timeframes.period.current}hrs</h2>
+            <span>Last Week - ${card.timeframes.period.previous}hrs</span>
+          </div>
+        </div>
+      </div>
     `
     html += htmlCards
   });
