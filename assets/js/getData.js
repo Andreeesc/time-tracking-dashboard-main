f1c0ea39e9018103714627e4bb9677bd05773e69
Andreeesc <de_s_c@hotmail.com>
2021-10-11 14:13:40 -0300

    fix: separando os arquivos js

---
+++ assets/js/getData.js
@@ -0,0 +1,13 @@
+async function getData(){
+  try{
+    const url = await fetch(`https://andreeesc.github.io/time-tracking-dashboard-main/data.json`)
+    console.log(url.status)
+    console.log(url.ok)
+
+    const response = await url.json()
+
+    return response
+  } catch(err){
+    console.error(err)
+  }
+}
